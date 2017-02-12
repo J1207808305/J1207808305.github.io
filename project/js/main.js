95178a6f2645c96e6375cb40c0488c73b666c86d
Johnny <1207808305@qq.com>
2017-02-13 00:23:44 +0800

    delete some unused images

--- project/js/main.js
+++ project/js/main.js
@@ -1,19 +1,5 @@
 jQuery(document).ready(function($) {
-
-    /* ======= Scrollspy ======= */
-    $('body').scrollspy({ target: '#header', offset: 400});
-    
-    /* ======= Fixed header when scrolled ======= */
-    
-    $(window).bind('scroll', function() {
-         if ($(window).scrollTop() > 50) {
-             $('#header').addClass('navbar-fixed-top');
-         }
-         else {
-             $('#header').removeClass('navbar-fixed-top');
-         }
-    });
-   
+  
     /* ======= ScrollTo ======= */
     $('a.scrollto').on('click', function(e){
         
